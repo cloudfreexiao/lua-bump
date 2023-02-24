b986fcbdee74f443a12acb8e1166445087a021a0
xiaojin <xiaojin@onemt.com.cn>
2023-02-24 11:35:59 +0800

    fix contains rect

--- 2d/bump2d.hpp
+++ 2d/bump2d.hpp
@@ -139,7 +139,7 @@ static bool rect_containsPoint(double x, double y, double w, double h,
 static bool rect_containsRect(double x1, double y1, double w1, double h1,
                               double x2, double y2, double w2, double h2)
 {
-    return (x2 >= x1) && (y2 >= y1) && (x1 + w1 >= x2 + w2) && (y1 + h1 >= y2 + h2);
+    return x1 <= x2 && y1 >= y2 && x1 + w1 <= x2 + w2 && y1 + h1 <= y2 + h2;
 }
 
 static bool rect_isIntersecting(double x1, double y1, double w1, double h1,
